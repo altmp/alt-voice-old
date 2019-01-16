5f18e57aaa6267ead451b158129338632ba196ed
Eugene Pogrebnyak <emcifuntik@live.com>
2019-01-16 13:57:15 +0300

    some checks for ci folders

--- vcbuild.bat
+++ vcbuild.bat
@@ -1,27 +1,30 @@
-@echo off
-echo Building opus library
-SET WinSDKVersion=10.0.17763.0
-msbuild ./vendor/opus/win32/VS2015/opus.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../../../libs
+REM @echo off
+REM echo Building opus library
+REM SET WinSDKVersion=10.0.17763.0
+REM msbuild ./vendor/opus/win32/VS2015/opus.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../../../libs/
 
-echo Building Portaudio library
-mkdir temp/portaudio
-cd temp/portaudio
-cmake -G "Visual Studio 15 Win64" ../../vendor/portaudio/
-cd ../../
-msbuild ./temp/portaudio/portaudio_static.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../libs
+REM echo Building Portaudio library
+REM mkdir temp/portaudio
+REM cd temp/portaudio
+REM cmake -G "Visual Studio 15 Win64" ../../vendor/portaudio/
+REM cd ../../
+REM msbuild ./temp/portaudio/portaudio_static.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../libs/
 
-echo Building OpenAL library
-mkdir temp/openal
-cd temp/openal
-cmake -G "Visual Studio 15 Win64" -DLIBTYPE=STATIC ../../vendor/openal/
-cd ../../
-msbuild ./temp/openal/OpenAL.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../libs
+REM echo Building OpenAL library
+REM mkdir temp/openal
+REM cd temp/openal
+REM cmake -G "Visual Studio 15 Win64" -DLIBTYPE=STATIC ../../vendor/openal/
+REM cd ../../
+REM msbuild ./temp/openal/OpenAL.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../libs/
 
-echo Building alt-voice library
-mkdir temp/alt-voice
-cd temp/alt-voice
-cmake -G "Visual Studio 15 Win64" ../../
-cd ../../
-msbuild ./temp/alt-voice/alt-voice.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../bin
+REM echo Building alt-voice library
+REM mkdir temp/alt-voice
+REM cd temp/alt-voice
+REM cmake -G "Visual Studio 15 Win64" ../../
+REM cd ../../
+REM msbuild ./temp/alt-voice/alt-voice.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../bin/
 
-echo alt-voice built
+REM echo alt-voice built
+
+
+dir

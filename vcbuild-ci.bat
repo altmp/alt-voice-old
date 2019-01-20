fd3fb3fa2fc94a279633a6466c15b2b9fbb87a6a
Eugene Pogrebnyak <emcifuntik@live.com>
2019-01-21 00:06:38 +0300

    ci finally works

--- vcbuild-ci.bat
+++ vcbuild-ci.bat
@@ -1,2 +1,10 @@
 call vcbuild.bat Debug
+if errorlevel 1 (
+   echo Debug build error
+   exit /b %errorlevel%
+)
 call vcbuild.bat Release
+if errorlevel 1 (
+   echo Release build error
+   exit /b %errorlevel%
+)
--- vcbuild.bat
+++ vcbuild.bat
@@ -6,6 +6,10 @@ cmake -G "Visual Studio 15 Win64" ../../vendor/opus/
 cd ../../
 call ".\vendor\opus\win32\genversion.bat" ".\vendor\opus\win32\version.h" PACKAGE_VERSION
 msbuild ./temp/opus/opus.vcxproj /p:PlatformToolset=v141 /p:Configuration=%1 /p:Platform=x64 /p:OutDir=../../libs/%1/
+if errorlevel 1 (
+   echo Opus build error
+   exit /b %errorlevel%
+)
 
 echo Building OpenAL library
 if not exist "./temp/openal" mkdir "./temp/openal"
@@ -13,6 +17,10 @@ cd ./temp/openal
 cmake -G "Visual Studio 15 Win64" -DLIBTYPE=STATIC -DALSOFT_BACKEND_DSOUND=OFF -DALSOFT_BACKEND_WASAPI=OFF  ../../vendor/openal/
 cd ../../
 msbuild ./temp/openal/OpenAL.vcxproj /p:PlatformToolset=v141 /p:Configuration=%1 /p:Platform=x64 /p:OutDir=../../libs/%1/
+if errorlevel 1 (
+   echo OpenAL build error
+   exit /b %errorlevel%
+)
 
 echo Building alt-voice library
 if not exist "./temp/alt-voice" mkdir "./temp/alt-voice"
@@ -20,7 +28,12 @@ cd ./temp/alt-voice
 cmake -DCMAKE_BUILD_TYPE=%1 -G "Visual Studio 15 Win64" ../../
 cd ../../
 msbuild ./temp/alt-voice/alt-voice.vcxproj /p:PlatformToolset=v141 /p:Configuration=%1 /p:Platform=x64 /p:OutDir=../../bin/%1/
+if errorlevel 1 (
+   echo alt-voice build error
+   exit /b %errorlevel%
+)
+
 if not exist "./bin/%1" mkdir "./bin/%1"
 copy /y .\temp\alt-voice\%1\alt-voice.lib .\bin\%1\alt-voice.lib
 
-echo "alt-voice built"
+echo alt-voice built

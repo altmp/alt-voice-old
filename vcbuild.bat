4c353a93edb24f3174969e69db6a24ccbaad9337
Eugene Pogrebnyak <emcifuntik@live.com>
2019-01-20 23:39:48 +0300

    build configuration

--- vcbuild.bat
+++ vcbuild.bat
@@ -4,21 +4,22 @@ if not exist "./temp/opus" mkdir "./temp/opus"
 cd ./temp/opus
 cmake -G "Visual Studio 15 Win64" ../../vendor/opus/
 cd ../../
-msbuild ./temp/opus/opus.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../libs/
+msbuild ./temp/opus/opus.vcxproj /p:PlatformToolset=v141 /p:Configuration=%1 /p:Platform=x64 /p:OutDir=../../libs/%1/
 
 echo Building OpenAL library
 if not exist "./temp/openal" mkdir "./temp/openal"
 cd ./temp/openal
-cmake -G "Visual Studio 15 Win64" -DLIBTYPE=STATIC ../../vendor/openal/
+cmake -G "Visual Studio 15 Win64" -DLIBTYPE=STATIC -DALSOFT_BACKEND_DSOUND=OFF -DALSOFT_BACKEND_WASAPI=OFF  ../../vendor/openal/
 cd ../../
-msbuild ./temp/openal/OpenAL.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../libs/
+msbuild ./temp/openal/OpenAL.vcxproj /p:PlatformToolset=v141 /p:Configuration=%1 /p:Platform=x64 /p:OutDir=../../libs/%1/
 
 echo Building alt-voice library
 if not exist "./temp/alt-voice" mkdir "./temp/alt-voice"
 cd ./temp/alt-voice
-cmake -G "Visual Studio 15 Win64" ../../
+cmake -DCMAKE_BUILD_TYPE=%1 -G "Visual Studio 15 Win64" ../../
 cd ../../
-msbuild ./temp/alt-voice/alt-voice.vcxproj /p:PlatformToolset=v141 /p:Configuration=Release /p:Platform=x64 /p:OutDir=../../bin/
-copy /y .\temp\alt-voice\Release\alt-voice.lib .\bin\alt-voice.lib
+msbuild ./temp/alt-voice/alt-voice.vcxproj /p:PlatformToolset=v141 /p:Configuration=%1 /p:Platform=x64 /p:OutDir=../../bin/%1/
+if not exist "./bin/%1" mkdir "./bin/%1"
+copy /y .\temp\alt-voice\%1\alt-voice.lib .\bin\%1\alt-voice.lib
 
 echo "alt-voice built"
